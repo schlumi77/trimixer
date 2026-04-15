27cb6db19e8365b04dd2a890087a491f795d41fa
Stephan Schlumbohm <stephan.schlumbohm@gmail.com>
2026-04-15 11:40:51 +0200

    corrected further paths again

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: './',
-  plugins: [
-    react()
-  ],
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+  return {
+    base: isBuild ? '/trimixer/' : '/',
+    plugins: [
+      react()
+    ],
+  };
 })
