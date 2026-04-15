3823533de51d47e4c165d95e46491876498b5fd9
Stephan Schlumbohm <stephan.schlumbohm@gmail.com>
2026-04-15 12:08:29 +0200

    fixed problems with relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
-  return {
-    base: isBuild ? '/trimixer/' : '/',
-    plugins: [
-      react()
-    ],
-  };
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icons.svg'],
+      manifest: {
+        name: 'Trimixer - Gas Blender',
+        short_name: 'Trimixer',
+        description: 'Technical Diving Gas Blending & Simulation Tool',
+        theme_color: '#00bcd4',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
