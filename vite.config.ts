8cd9c70203944d3145a6c76d72e5b0187bd9c7a7
Stephan Schlumbohm <stephan.schlumbohm@gmail.com>
2026-04-15 12:45:57 +0200

    fixed build errors

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig(({ command }) => {
-  const isBuild = command === 'build';
-  return {
-    base: './',
-    plugins: [
-      react(),
-      VitePWA({
-        registerType: 'autoUpdate',
-        includeAssets: ['favicon.svg', 'icons.svg'],
-        manifest: {
-          name: 'Trimixer - Gas Blender',
-          short_name: 'Trimixer',
-          description: 'Technical Diving Gas Blending & Simulation Tool',
-          theme_color: '#00bcd4',
-          icons: [
-            {
-              src: 'favicon.svg',
-              sizes: 'any',
-              type: 'image/svg+xml'
-            }
-          ]
-        }
-      })
-    ],
-  };
+export default defineConfig({
+  base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icons.svg'],
+      manifest: {
+        name: 'Trimixer - Gas Blender',
+        short_name: 'Trimixer',
+        description: 'Technical Diving Gas Blending & Simulation Tool',
+        theme_color: '#00bcd4',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          }
+        ]
+      }
+    })
+  ],
 })
